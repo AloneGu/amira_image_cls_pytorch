ac4a283db67cae7eccf7bdd6009501d2bd029c78
Wei Gu <wg@gizwits.com>
2017-08-14 17:25:32 +0800

    lenet work good

--- img_cls/apps.py
+++ img_cls/apps.py
@@ -28,7 +28,7 @@ def setcfg(name, default=''):
 
 setcfg('IMG_WIDTH', 224)
 setcfg('IMG_HEIGHT', 224)
-setcfg('MODEL_NAME', "ALEXNET")
+setcfg('MODEL_NAME', "LENET")
 setcfg('EPOCH', 5)
 setcfg('DATA_DIR', '../data/hive_bk')
 
